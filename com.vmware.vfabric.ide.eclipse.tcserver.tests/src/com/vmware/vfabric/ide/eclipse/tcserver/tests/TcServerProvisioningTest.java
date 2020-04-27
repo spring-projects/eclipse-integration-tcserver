a4e44dbce91d6f115e9f864aa28208105b0c5eed
BoykoAlex <aboyko@pivotal.io>
2020-04-27 14:51:40 -0400

    Remove debug code

--- com.vmware.vfabric.ide.eclipse.tcserver.tests/src/com/vmware/vfabric/ide/eclipse/tcserver/tests/TcServerProvisioningTest.java
+++ com.vmware.vfabric.ide.eclipse.tcserver.tests/src/com/vmware/vfabric/ide/eclipse/tcserver/tests/TcServerProvisioningTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Pivotal Software, Inc.
+ * Copyright (c) 2012, 2020 Pivotal Software, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@ package com.vmware.vfabric.ide.eclipse.tcserver.tests;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -67,28 +66,22 @@ public class TcServerProvisioningTest {
 	@Test
 	public void testCreateServer() throws Exception {
 		Assume.assumeNotNull(fixture.getDownloadUrl());
-//		try {
-			IServer server = null;
-			try {
-				server = harness.createServer(null);
-			} catch (Exception e) {
-				e.printStackTrace();
-				fail(e.getMessage());
-			}
+		try {
+			IServer server = harness.createServer(null);
 			assertNotNull(server);
 		}
-//		catch (CoreException e) {
-//			// on Java 1.5 provisioning is expected to fail with tc Server v2.5
-//			// or later
-//			if (e.getMessage()
-//					.equals("tc Server v2.5 or later requires Java SE 6 or later.  Change the JRE to one that meets this requirement.")) {
-//				assertTrue("Expected Java version < 1.6, got: " + System.getProperty("java.version"), System
-//						.getProperty("java.version").compareTo("1.6") < 0);
-//			}
-//			else {
-//				throw e;
-//			}
-//		}
-//	}
+		catch (CoreException e) {
+			// on Java 1.5 provisioning is expected to fail with tc Server v2.5
+			// or later
+			if (e.getMessage()
+					.equals("tc Server v2.5 or later requires Java SE 6 or later.  Change the JRE to one that meets this requirement.")) {
+				assertTrue("Expected Java version < 1.6, got: " + System.getProperty("java.version"), System
+						.getProperty("java.version").compareTo("1.6") < 0);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
 
 }
